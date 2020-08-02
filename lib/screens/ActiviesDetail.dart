cae927bd320fa2ea4698e8e644ab7685a530f2e9
Christwide Oscar <Aviliusedme@gmail.com>
2020-08-02 10:41:49 -0500

    Update to Activites, creation of Boxes with description and button that will lead to full details

--- lib/screens/ActiviesDetail.dart
+++ lib/screens/ActiviesDetail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kidoresources/constants.dart';
 import 'package:kidoresources/screens/CalenderScreen.dart';
 import 'package:kidoresources/screens/HomeScreen.dart';
 import 'package:kidoresources/widgets/Category_Card.dart';
@@ -50,7 +51,7 @@ class ActiviesDetail extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           Container(
-            height: size.height * .55,
+            height: size.height * .75,
             decoration: BoxDecoration(
               color: Colors.lightBlue[200],
               image: DecorationImage(
@@ -118,7 +119,8 @@ class ActiviesDetail extends StatelessWidget {
                     SizedBox(height: 10),
                     SizedBox(height: 10),
                     SizedBox(
-                      width: size.width * .62, // it just take 60% of total width
+                      width:
+                          size.width * .62, // it just take 60% of total width
                       child: Text(
                         "Live happoer and healthier by having fun and keeping active.\n"
                         "Fun Fact: Working out and having fun sharpens memory and brain activity!",
@@ -128,10 +130,102 @@ class ActiviesDetail extends StatelessWidget {
                       width: size.width * .5, // it just take the 50% width
                       child: SearchBar(),
                     ),
-                    SizedBox(height: 20),
-                    Text(
-                      "DCCDS",
-                      style: TextStyle(fontWeight: FontWeight.w900),
+                    SizedBox(height: 10),
+                    // DCCDS
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      padding: EdgeInsets.all(10),
+                      height: 165,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(13),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 17),
+                            blurRadius: 23,
+                            spreadRadius: -13,
+                            color: kShadowColor,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          SvgPicture.asset(
+                            "assets/icons/Meditation_women_small.svg",
+                          ),
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "DCCDS",
+                                  style: Theme.of(context).textTheme.subtitle,
+                                ),
+                                Text(
+                                    "Everyday a child walks thorugh the door and experiances change."
+                                    " Let us be the first thing they see. Growing together to make amaerica great again.")
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(5),
+                            child: IconButton(
+                              icon: Icon(Icons.chevron_right),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Another Container
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10),
+                      padding: EdgeInsets.all(10),
+                      height: 165,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(13),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 17),
+                            blurRadius: 23,
+                            spreadRadius: -13,
+                            color: kShadowColor,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          SvgPicture.asset(
+                            "assets/icons/Meditation_women_small.svg",
+                          ),
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "DCCDS",
+                                  style: Theme.of(context).textTheme.subtitle,
+                                ),
+                                Text(
+                                    "Everyday a child walks thorugh the door and experiances change."
+                                    " Let us be the first thing they see. Growing together to make amaerica great again.")
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(5),
+                            child: IconButton(
+                              icon: Icon(Icons.chevron_right),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
