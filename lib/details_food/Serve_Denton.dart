67124c76d5107ef759b6f28e470dbe20f1822e73
Christwide Oscar <Aviliusedme@gmail.com>
2020-08-25 23:45:14 -0500

    Updates to files and backend

--- lib/details_food/Serve_Denton.dart
+++ lib/details_food/Serve_Denton.dart
@@ -1,52 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:kidoresources/constants.dart';
-import 'package:kidoresources/screens/CalenderScreen.dart';
-import 'package:kidoresources/screens/HomeScreen.dart';
-import 'package:kidoresources/widgets/Navigation_bar.dart';
+import 'package:kidoresources/screens/FoodDetail.dart';
 
 class ServeDenton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Bottom Nav bar: Beginning
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.symmetric(horizontal: 70, vertical: 10),
-        height: 70,
-        color: Colors.grey[200],
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            BottomNavigation(
-              title: "Home",
-              svgSrc: "assets/icons/Icon_home.svg",
-              press: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) {
-                    return HomeScreen();
-                  }),
-                );
-              },
-              isActive: false,
-            ),
-            BottomNavigation(
-              title: "Today",
-              svgSrc: "assets/icons/Icon_calendar.svg",
-              press: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) {
-                    return CalenderScreen();
-                  }),
-                );
-              },
-              isActive: false,
-            ),
-          ],
-        ),
-      ),
-      // Bottom Nav bar: Ending
-
       body: Stack(
         children: [
           Container(
@@ -71,7 +30,7 @@ class ServeDenton extends StatelessWidget {
                       Navigator.push(
                         context,
                         MaterialPageRoute(builder: (context) {
-                          return CalenderScreen();
+                          return FoodDetail();
                         }),
                       );
                     }),
@@ -295,7 +254,6 @@ class ServeDenton extends StatelessWidget {
                       ),
                     ),
                     //Our Soulution
-
                     Container(
                       margin: EdgeInsets.symmetric(vertical: 5),
                       padding: EdgeInsets.all(10),
