e36e6ae023acdeeeef91fca7939a21ddaabce474
Christwide Oscar <Aviliusedme@gmail.com>
2020-09-14 11:31:12 -0500

    Creation of custom Widget for information screens

--- lib/details_food/FRM.dart
+++ lib/details_food/FRM.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:kidoresources/constants.dart';
+import 'package:kidoresources/widgets/Information_Card.dart';
 
 class FirstRefugeMinistries2 extends StatelessWidget {
   @override
@@ -37,7 +39,42 @@ class FirstRefugeMinistries2 extends StatelessWidget {
           SafeArea(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 30),
-              child: SingleChildScrollView(),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 45,
+                    ),
+                    //Our Mission
+                    InfoCard(
+                      cardheight: 250.0,
+                      title: "Our Mission",
+                      svgImage: "assets/icons/Healthcare_people.svg",
+                      text:
+                          "Serve Denton partners with nonprofits to help make thier service more accessible for people in need.",
+                    ),
+                    SizedBox(height: 15),
+                    //Our Mission
+                    InfoCard(
+                      cardheight: 250.0,
+                      title: "Our Mission",
+                      svgImage: "assets/icons/Healthcare_people.svg",
+                      text:
+                          "Serve Denton partners with nonprofits to help make thier service more accessible for people in need.",
+                    ),
+                    SizedBox(height: 15),
+                    //Our Mission
+                    InfoCard(
+                      cardheight: 250.0,
+                      title: "Our Mission",
+                      svgImage: "assets/icons/Healthcare_people.svg",
+                      text:
+                          "Serve Denton partners with nonprofits to help make thier service more accessible for people in need.",
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ],
